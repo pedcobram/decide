dde7b40a3eadbe7fdd8ba4048abc69193bd9c122
PedroC <pedcobram@alum.us.es>
2020-01-10 11:54:46 +0100

    test

--- decide/census/migrations/0002_auto_20191216_1512.py
+++ decide/census/migrations/0002_auto_20191216_1512.py
@@ -17,11 +17,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='census',
-            name='localidad',
-            field=models.CharField(max_length=20, null=True),
-        ),
-        migrations.AddField(
-            model_name='census',
             name='provincia',
             field=models.CharField(max_length=20, null=True),
         ),
--- decide/census/migrations/0004_merge_20200110_1000.py
+++ decide/census/migrations/0004_merge_20200110_1000.py
@@ -7,7 +7,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('census', '0003_auto_20191217_2049'),
-        ('census', '0002_auto_20200103_1227'),
         ('census', '0002_auto_20191226_1908'),
 	('census', '0002_auto_20191216_1512'),
     ]
