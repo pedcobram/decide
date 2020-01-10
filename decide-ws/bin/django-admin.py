cc6411f1f5e3b1e76279b888732a0124fb642c49
PedroC <pedcobram@alum.us.es>
2020-01-10 11:17:02 +0100

    < Errores : Arreglado htmls >
    - Arreglado error en el merge -

--- decide-ws/bin/django-admin.py
+++ decide-ws/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/pedroc/Documentos/decide/decide/decide-ws/bin/python3
+#!/home/pedroc/Documentos/decide/decide-ws/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
