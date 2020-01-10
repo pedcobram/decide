721ac2750d664182dda9fda2efabfa75d2c4306a
PedroC <pedcobram@alum.us.es>
2020-01-10 11:46:08 +0100

    Cambiado archivo bd

--- decide/census/migrations/0004_merge_20200110_1000.py
+++ decide/census/migrations/0004_merge_20200110_1000.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
         ('census', '0003_auto_20191217_2049'),
         ('census', '0002_auto_20200103_1227'),
         ('census', '0002_auto_20191226_1908'),
+	('census', '0002_auto_20191216_1512'),
     ]
 
     operations = [
