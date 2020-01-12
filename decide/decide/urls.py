3cb094350dfdba13c984dc505358307c51099275
PedroC <pedcobram@alum.us.es>
2020-01-12 23:27:43 +0100

    < Reset: Reset a commit funcional >

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from census.views import census_display, census_create_by_city,census_delete_by_city,census_create_by_localidad, census_delete_by_localidad ,census_create_by_age,census_delete_by_age, census_create_by_genero, census_delete_by_genero
+from census.views import census_copy
 
+from census.views import census_display
+from census.views import census_upload
 
 schema_view = get_swagger_view(title='Decide API')
 
@@ -26,17 +28,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
+    path('admin/census-copy/',census_copy),
     path('admin/census-display/',census_display),
-    path('census_create_by_city/<int:voting_id>/<str:provincia>/', census_create_by_city, name='census_create_by_city'),
-    path('census_delete_by_city/<str:provincia>/', census_delete_by_city, name='census_delete_by_city'),
-    path('census_create_by_localidad/<int:voting_id>/<str:localidad>/', census_create_by_localidad, name='census_create_by_localidad'),
-    path('census_delete_by_localidad/<str:localidad>/', census_delete_by_localidad, name='census_delete_by_localidad'),
-    path('census_create_by_age/<int:voting_id>/<int:edad_minima>/', census_create_by_age, name='census_create_by_age'),
-    path('census_delete_by_age/<int:edad_minima>/', census_delete_by_age, name='census_delete_by_age'),
-    path('census_create_by_genero/<int:voting_id>/<str:genero>/', census_create_by_genero, name='census_create_by_age'),
-    path('census_delete_by_genero/<str:genero>/', census_delete_by_genero, name='census_delete_by_genero'),
-
-
+    path('census-upload/', census_upload, name="census_upload"),
 ]
 
 for module in settings.MODULES:
--- decide/test.py
+++
@@ -1,4 +0,0 @@
-voting_id,voter_id,fecha_nacimiento,genero,provincia,localidad
-1,2,1990-12-20,Femenino,Sevilla,Sevilla
-1,3,2000-12-20,Femenino,Sevilla,Sevilla
-1,4,2013-12-20,Femenino,Cordoba,Cordoba
