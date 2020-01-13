9f809634b16799dc74273e6bcaa204dd7b7c0684
PedroC <pedcobram@alum.us.es>
2020-01-13 11:33:14 +0100

    defensa test commit

---
+++ decide/test.py
@@ -0,0 +1,4 @@
+voting_id,voter_id,fecha_nacimiento,genero,provincia,localidad
+1,2,1990-12-20,Femenino,Sevilla,Sevilla
+1,3,2000-12-20,Femenino,Sevilla,Sevilla
+1,4,2013-12-20,Femenino,Cordoba,Cordoba
